b8f43e5908d3381b2785aa560b20d2572cf8bee7
FredWolff <frederik.wolff00@gmail.com>
2022-08-19 21:27:00 +0200

    System specified by class Sys

--- VerletSolver.py
+++ VerletSolver.py
@@ -1,4 +1,6 @@
+from re import L
 import jax.numpy as jnp
+from jax import lax.scan as scan
 import matplotlib.pyplot as plt
 
 def verlet_solver(rhs, ts, y0, v0):
@@ -19,11 +21,11 @@ def verlet_solver(rhs, ts, y0, v0):
 
     """
     dt = ts[1] - ts[0]  # step size
-    ys = jnp.zeros(shape=(ts.size, y0.size))  # initiate position array
-    vs = jnp.zeros(shape=(ts.size, v0.size))  # initiate velocity array
+    # ys = jnp.zeros(shape=(ts.size, y0.size))  # initiate position array
+    # vs = jnp.zeros(shape=(ts.size, v0.size))  # initiate velocity array
 
-    ys = ys.at[0].set(y0)
-    vs = vs.at[0].set(v0)
+    # ys = ys.at[0].set(y0)
+    # vs = vs.at[0].set(v0)
     # for i in range(len(ts) - 1):
     #     accs1 = rhs(ys[i], vs[i])
     #     v = vs[i] + 0.5 * accs1 * dt
@@ -31,28 +33,42 @@ def verlet_solver(rhs, ts, y0, v0):
     #     accs2 = rhs(ys[i + 1], v)
     #     vs = vs.at[i + 1].set(v + 0.5 * (accs2 - accs1) * dt)
 
-    def step(index, x, v, theta, omega):
-
-
-    
+    _, (x, v, theta, omega)
 
     return ys, vs
 
 
-def x_acc(F, theta, omega, alpha):
-    return (F + m * l * (alpha * jnp.cos(theta) - omega**2 * jnp.sin(theta))) / (M + m)
+############## System ##############
+def x_acc(force, theta, omega, alpha):
+    return (force + m * l * (alpha * jnp.cos(theta) - omega**2 * jnp.sin(theta))) / (M + m)
 
 
 def theta_acc(a, theta):
     return (g * jnp.sin(theta) + a * jnp.cos(theta)) / l
 
 
-def get_acc(F, a, theta, omega, alpha):
-    return x_acc(F, theta, omega, alpha), theta_acc(a, theta)
+def get_acc(force, a, theta, omega, alpha):
+    return x_acc(force, theta, omega, alpha), theta_acc(a, theta)
+
+
+class Sys():
+    def __init__(self, l, m, M):
+        self.l = l
+        self.m = m
+        self.M = M
+        
+        
+    def __repr__(self):
+        return f'Sys(l={self.l}, m={self.m}, M={self.M})'
+    
+    
+    def __str__(self):
+        return f'Pendulum lenght is {self.l} m, pendulum mass is {self.m} kg, cart mass is {self.M} kg'
 
 
 def rhs(ys, vs):
     return jnp.array([0, -1])
+    
 
 if __name__ == '__main__':
     ts = jnp.linspace(0, 6, 100)
